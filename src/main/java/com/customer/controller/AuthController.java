63e9904db70cf568b1087582f26a243cd2d4dc39
shrsaurav002 <67121894+shrsaurav002@users.noreply.github.com>
2020-08-08 23:22:04 -0400

    DO NOT CHANGE

---
+++ src/main/java/com/customer/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.customer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class AuthController {
+	@GetMapping({ "/", "/auth" })
+	public String authform() {
+		return "index";
+	}
+
+	@PostMapping("/auth")
+	public String authUser(@RequestParam String usernameOrEmail, @RequestParam String password) {
+		// CustomerDTO customerDTO = customerService.authUser(usernameOrEmail,password);
+
+		/*
+		 * if (true) return "dashboard"; else return "error";
+		 */
+		return "success";
+
+	}
+}
